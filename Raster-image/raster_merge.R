3a57fe6a555bdf91f9aed1dc5f002ce5cc532e21
VTM-Planfarm <vinh@planfarm.com.au>
2025-06-26 14:29:19 +0800

    Add raster merging functionality with masking based on geojson input

---
+++ Raster-image/raster_merge.R
@@ -0,0 +1,21 @@
+renv::snapshot()
+
+library(sf, quietly = TRUE)
+library(terra, quietly = TRUE)
+
+tif_files <- list.files("Raster-image/tif-input", full.names = TRUE,
+                        pattern = "*.tif$", recursive = TRUE)
+
+rasters <- lapply(tif_files, rast)
+
+merged_raster <- do.call(merge, rasters)
+
+forest_block_path <- Sys.glob("Raster-image/forest-block/*.geojson")
+
+sf_forest_block <- st_read(forest_block_path)
+
+sf_forest_block <- st_transform(sf_forest_block, 4326)
+
+masked_raster <- mask(merged_raster, sf_forest_block, inverse = TRUE)
+
+writeRaster(masked_raster, "Raster-image/output/merged_interpolation.tif")
